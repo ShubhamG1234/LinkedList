f09ebaafc48a4dfdd22ba0e204bf2c6c3446975e
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-10 18:34:41 +0530

    initial commit

---
+++ LinkList/LinkList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Linked_List<int> mylist = new Linked_List<int>();
+
+
+
+            mylist.Add(56);
+            mylist.Add(30);
+            mylist.Add(70);
+            mylist.Display();
+        }
+    }
+}
